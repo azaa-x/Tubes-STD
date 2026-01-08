ad492779949e63a8b996fea5671410216603e517
azaa-x <n.andrean119@gmail.com>
2026-01-08 22:01:12 +0700

    update code

--- Product.cpp
+++ Product.cpp
@@ -1,108 +1,26 @@
-#include "Product.h"
+#ifndef PRODUCT_H_INCLUDED
+#define PRODUCT_H_INCLUDED
+
 #include <iostream>
 using namespace std;
 
-adrNode createNode(string name, int jumlah) {
-    adrNode p = new Product;
-    p->name = name;
-    p->jumlah = jumlah;
-    p->left = nullptr;
-    p->right = nullptr;
-    return p;
-}
+typedef struct Product* adrNode;
 
-void insertProduct(adrNode &root, adrNode p) {
-    if (root == nullptr) {
-        root = p;
-    } else {
-        if (p->jumlah < root->jumlah) {
-            insertProduct(root->left, p);
-        } else {
-            insertProduct(root->right, p);
-        }
-    }
-}
+struct Product {
+    string name;
+    int jumlah;
+    adrNode left;
+    adrNode right;
+};
 
-adrNode searchProduct(adrNode root, int jumlah) {
-    if (root == nullptr || root->jumlah == jumlah) {
-        return root;
-    } else {
-        if (jumlah < root->jumlah) {
-            return searchProduct(root->left, jumlah);
-        } else {
-            return searchProduct(root->right, jumlah);
-        }
-    }
-}
+adrNode createNode(string name, int jumlah);
+void insertProduct(adrNode &root, adrNode p);
+adrNode searchProduct(adrNode root, int jumlah);
+void inOrder(adrNode root);
+void preOrder(adrNode root);
+void postOrder(adrNode root);
+adrNode deleteNode(adrNode root, int jumlah);
+int getMinValue(adrNode root);
+int getMaxValue(adrNode root);
 
-void preOrder(adrNode root) {
-    if (root != nullptr) {
-        cout << root->name << " (jumlah: " << root->jumlah << ")\n";
-        preOrder(root->left);
-        preOrder(root->right);
-    }
-}
-
-void inOrder(adrNode root) {
-    if (root != nullptr) {
-        inOrder(root->left);
-        cout << root->name << " (jumlah: " << root->jumlah << ")\n";
-        inOrder(root->right);
-    }
-}
-
-void postOrder(adrNode root) {
-    if (root != nullptr) {
-        postOrder(root->left);
-        postOrder(root->right);
-        cout << root->name << " (jumlah: " << root->jumlah << ")\n";
-    }
-}
-
-int getMinValue(adrNode root) {
-    if (root == nullptr) {
-        return -1;
-    } else {
-        while (root->left != nullptr) {
-            root = root->left;
-        }
-        return root->jumlah;
-    }
-}
-
-int getMaxValue(adrNode root) {
-    if (root == nullptr) {
-        return -1;
-    } else {
-        while (root->right != nullptr) {
-            root = root->right;
-        }
-        return root->jumlah;
-    }
-}
-
-adrNode deleteNode(adrNode root, int jumlah) {
-    if (root == nullptr) {
-        return nullptr;
-    } else {
-        if (jumlah < root->jumlah) {
-            root->left = deleteNode(root->left, jumlah);
-        } else if (jumlah > root->jumlah) {
-            root->right = deleteNode(root->right, jumlah);
-        } else {
-            if (root->left == nullptr && root->right == nullptr) {
-                delete root;
-                return nullptr;
-            } else if (root->left == nullptr) {
-                adrNode temp = root->right;
-                delete root;
-                return temp;
-            } else if (root->right == nullptr) {
-                adrNode temp = root->left;
-                delete root;
-                return temp;
-            }
-        }
-        return root;
-    }
-}
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Product.h"
-#include <iostream>
+#include <iostream>>
+
 using namespace std;
 
 int main() {
@@ -7,13 +8,14 @@ int main() {
     int pilihan;
 
     do {
-        cout << "\n=== MENU PRODUCT BST ===\n";
+        cout << "\n=== MENU PRODUCT MARKETPLACE (BST) ===\n";
         cout << "1. Tambah Produk\n";
         cout << "2. Hapus Produk\n";
-        cout << "3. Tampilkan Produk (Inorder)\n";
-        cout << "4. Tampilkan Produk (Preorder)\n";
-        cout << "5. Tampilkan Produk (Postorder)\n";
-        cout << "6. Tampilkan Produk Min/Max\n";
+        cout << "3. Tampilkan Inorder\n";
+        cout << "4. Tampilkan Preorder\n";
+        cout << "5. Tampilkan Postorder\n";
+        cout << "6. Cari Produk\n";
+        cout << "7. Tampilkan Nilai Penjualan Minimum dan Maksimum\n";
         cout << "0. Keluar\n";
         cout << "Pilih: ";
         cin >> pilihan;
@@ -22,62 +24,71 @@ int main() {
             case 1: {
                 string name;
                 int jumlah;
-
                 cout << "Nama Produk: ";
                 cin >> name;
                 cout << "Jumlah Penjualan: ";
                 cin >> jumlah;
-
                 insertProduct(root, createNode(name, jumlah));
+                cout << "Produk berhasil ditambahkan! " << endl;
                 break;
             }
+
             case 2: {
                 int jumlah;
-                cout << "Masukkan jumlah penjualan produk yang ingin dihapus: ";
+                cout << "Masukkan jumlah penjualan yang dihapus: ";
                 cin >> jumlah;
                 root = deleteNode(root, jumlah);
+                cout << "Data berhasil diproses\n";
                 break;
             }
-            case 3: {
-                cout << "\n=== Inorder Traversal ===\n";
+
+            case 3:
+                cout << "\n--- Inorder Traversal ---\n";
                 inOrder(root);
                 break;
-            }
-            case 4: {
-                cout << "\n=== Preorder Traversal ===\n";
+
+            case 4:
+                cout << "\n--- Preorder Traversal ---\n";
                 preOrder(root);
                 break;
-            }
-            case 5: {
-                cout << "\n=== Postorder Traversal ===\n";
+
+            case 5:
+                cout << "\n--- Postorder Traversal ---\n";
                 postOrder(root);
                 break;
-            }
+
             case 6: {
-                if (root == nullptr) {
-                    cout << "Tree kosong!\n";
+                int jumlah;
+                cout << "Cari jumlah penjualan: ";
+                cin >> jumlah;
+                adrNode hasil = searchProduct(root, jumlah);
+                if (hasil != nullptr) {
+                    cout << "Produk ditemukan\n";
+                    cout << "Nama   : " << hasil->name << endl;
+                    cout << "Jumlah : " << hasil->jumlah << endl;
                 } else {
-                    int minJumlah = getMinValue(root);
-                    int maxJumlah = getMaxValue(root);
-
-                    adrNode minNode = searchProduct(root, minJumlah);
-                    adrNode maxNode = searchProduct(root, maxJumlah);
-
-                    cout << "\nProduk dengan jumlah penjualan MIN: "
-                         << minNode->name << " (" << minNode->jumlah << ")\n";
-                    cout << "Produk dengan jumlah penjualan MAX: "
-                         << maxNode->name << " (" << maxNode->jumlah << ")\n";
+                    cout << "Produk tidak ditemukan\n";
                 }
                 break;
             }
-            case 0: {
+
+            case 7:
+                if (root == nullptr) {
+                    cout << "Tree masih kosong\n";
+                } else {
+                    cout << "Penjualan Minimum : "
+                         << getMinValue(root) << endl;
+                    cout << "Penjualan Maksimum: "
+                         << getMaxValue(root) << endl;
+                }
+                break;
+
+            case 0:
                 cout << "Keluar...\n";
                 break;
-            }
-            default: {
-                cout << "Pilihan tidak tersedia!\n";
-                break;
-            }
+
+            default:
+                cout << "Pilihan salah!\n";
         }
 
     } while (pilihan != 0);
