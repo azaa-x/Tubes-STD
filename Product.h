c6c1ca584b30db25d1773874ddc1d5a26ddd42e2
azaa-x <n.andrean119@gmail.com>
2026-01-09 10:17:33 +0700

    update code

--- Product.h
+++ Product.h
@@ -1,121 +1,26 @@
-#include "Product.h"
-#include <iostream>"
+#ifndef PRODUCT_H_INCLUDED
+#define PRODUCT_H_INCLUDED
 
+#include <iostream>
 using namespace std;
 
-adrNode createNode(string name, int jumlah) {
-    adrNode p = new Product;
-    p->name = name;
-    p->jumlah = jumlah;
-    p->left = nullptr;
-    p->right = nullptr;
-    return p;
-}
+typedef struct Product* adrNode;
 
-void insertProduct(adrNode &root, adrNode p) {
-    if (root == nullptr) {
-        root = p;
-    } else if (p->jumlah < root->jumlah) {
-        insertProduct(root->left, p);
-    } else {
-        insertProduct(root->right, p);
-    }
-}
+struct Product {
+    string name;
+    int jumlah;
+    adrNode left;
+    adrNode right;
+};
 
-adrNode searchProduct(adrNode root, int jumlah) {
-    if (root == nullptr || root->jumlah == jumlah) {
-        return root;
-    } else if (jumlah < root->jumlah) {
-        return searchProduct(root->left, jumlah);
-    } else {
-        return searchProduct(root->right, jumlah);
-    }
-}
+adrNode createNode(string name, int jumlah);
+void insertProduct(adrNode &root, adrNode p);
+adrNode searchProduct(adrNode root, int jumlah);
+void inOrder(adrNode root);
+void preOrder(adrNode root);
+void postOrder(adrNode root);
+adrNode deleteNode(adrNode root, int jumlah);
+int getMinValue(adrNode root);
+int getMaxValue(adrNode root);
 
-void inOrder(adrNode root) {
-    if (root != nullptr) {
-        inOrder(root->left);
-        cout << root->name << " (" << root->jumlah << ")\n";
-        inOrder(root->right);
-    }
-}
-
-void preOrder(adrNode root) {
-    if (root != nullptr) {
-        cout << root->name << " (" << root->jumlah << ")\n";
-        preOrder(root->left);
-        preOrder(root->right);
-    }
-}
-
-void postOrder(adrNode root) {
-    if (root != nullptr) {
-        postOrder(root->left);
-        postOrder(root->right);
-        cout << root->name << " (" << root->jumlah << ")\n";
-    }
-}
-
-int getMinValue(adrNode root) {
-    if (root == nullptr) {
-        return -1;
-    }
-
-    while (root->left != nullptr) {
-        root = root->left;
-    }
-
-    return root->jumlah;
-}
-
-int getMaxValue(adrNode root) {
-    if (root == nullptr) {
-        return -1;
-    }
-
-    while (root->right != nullptr) {
-        root = root->right;
-    }
-
-    return root->jumlah;
-}
-
-adrNode deleteNode(adrNode root, int jumlah) {
-    if (root == nullptr) {
-        return nullptr;
-    }
-
-    if (jumlah < root->jumlah) {
-        root->left = deleteNode(root->left, jumlah);
-    }
-    else if (jumlah > root->jumlah) {
-        root->right = deleteNode(root->right, jumlah);
-    }
-    else {
-        // node tidak memiliki anak
-        if (root->left == nullptr && root->right == nullptr) {
-            delete root;
-            return nullptr;
-        }
-        // node memiliki satu anak kanan
-        else if (root->left == nullptr) {
-            adrNode temp = root->right;
-            delete root;
-            return temp;
-        }
-        // node memiliki satu anak kiri
-        else if (root->right == nullptr) {
-            adrNode temp = root->left;
-            delete root;
-            return temp;
-        }
-        // node memiliki dua anak
-        else {
-            int successor = getMinValue(root->right);
-            root->jumlah = successor;
-            root->right = deleteNode(root->right, successor);
-        }
-    }
-
-    return root;
-}
+#endif
